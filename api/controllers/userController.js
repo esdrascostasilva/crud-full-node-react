501f35bd7d4f7db2b2812e402bd9946cdf161f10
esdrascosta <esdras.cts@outlook.com>
2024-06-03 15:55:33 -0300

    fact: Adding function to create (post) user

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -10,6 +10,24 @@ export const getUsers = (_, response) =>
 
         return response.status(200).json(data)
     })
+};
 
+export const addUser = (request, response) =>
+{
+    const query = "INSERT INTO usuarios(`name`, `email`, `phone`, `birth_date`) VALUES(?);"
 
-}
+    const values = 
+    [
+        request.body.name,
+        request.body.email,
+        request.body.phone,
+        request.body.birth_date,
+    ];
+
+    database.query(query, [values], (error) => 
+    {
+        if(error) return response.json(error);
+
+        return response.status(201).json("Usuario cadastrado com sucesso.")
+    })
+};
