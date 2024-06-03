b86e49e3af0baf5025a941ce84830c56188071e3
esdrascosta <esdras.cts@outlook.com>
2024-06-03 16:57:40 -0300

    feat: Adding actions methods to GetById, Post, Put and Delete on Controller.

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -12,6 +12,18 @@ export const getUsers = (_, response) =>
     })
 };
 
+export const getUsersById = (request, response) => 
+{
+    const query = "SELECT * FROM usuarios WHERE `id` = ?;"
+
+    database.query(query, [request.params.id], (error, data) => 
+    {
+        if(error) return response.json(error)
+
+        return response.status(200).json(data)
+    })
+};
+
 export const addUser = (request, response) =>
 {
     const query = "INSERT INTO usuarios(`name`, `email`, `phone`, `birth_date`) VALUES(?);"
@@ -29,5 +41,37 @@ export const addUser = (request, response) =>
         if(error) return response.json(error);
 
         return response.status(201).json("Usuario cadastrado com sucesso.")
+    })
+};
+
+export const updateUser = (request, response) =>
+{
+    const query = "UPDATE usuarios SET `name` = ?, `email` = ?, `phone` = ?, `birth_date` = ? WHERE `id` = ?;"
+
+    const values = 
+    [
+        request.body.name,
+        request.body.email,
+        request.body.phone,
+        request.body.birth_date,
+    ]
+    
+    database.query(query, [...values, request.params.id], (error) => 
+    {
+        if(error) return response.json(error)
+
+        return response.status(200).json("Usuario atualizado com sucesso.")
+    })
+};
+
+export const deleteUser = (request, response) => 
+{
+    const query = "DELETE FROM usuarios WHERE `id` = ?;"
+
+    database.query(query, [request.params.id], (error) =>
+    {
+        if(error) return response.json(error)
+
+        return response.status(200).json("Usuario deletado com sucesso")
     })
 };
