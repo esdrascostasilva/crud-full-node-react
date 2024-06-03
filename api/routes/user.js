b6863f3b62a987ae9e2a35bec5a3131462c706eb
esdrascosta <esdras.cts@outlook.com>
2024-06-03 15:55:55 -0300

    feat: Adding route to create of user

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getUsers } from "../controllers/userController.js";
+import { getUsers, addUser } from "../controllers/userController.js";
 
 const router = express.Router()
 
 router.get("/users", getUsers)
+router.post("/users", addUser)
 
 
 export default router;
